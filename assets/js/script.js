4efed302d3283bb28c4dacac426013359a3efe74
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2021-12-30 13:18:11 +0000

    Correct mistakes shown from jshint

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('questions').innerText = 'Welcome to Quizzical, please click start to continue'
-})
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('questions').innerText = 'Welcome to Quizzical, please click start to continue';
+});
 
 //variables
 const letsStart = document.getElementById('start-btn');
@@ -9,9 +9,6 @@ const questionCont = document.getElementById('questions-cont');
 const questionArea = document.getElementById('questions');
 const answersArea = document.getElementById('answer-choice');
 const restartButton = document.getElementById('restart-btn');
-const resultCont = document.getElementById('results-cont');
-const gameArea = document.getElementsByClassName('game-area');
-const heading = document.getElementsByTagName('h1');
 let shuffledQuestions; //hold the questions that are random
 let currentQuestionIndex; //index for the current question
 let currentScore = 0;
@@ -23,9 +20,9 @@ letsStart.addEventListener('click', runGame);
 nextButton.addEventListener('click', () => {
     currentQuestionIndex++;
     getNextQuestion();
-})
+});
 
-restartButton.onclick = function() {
+restartButton.onclick = function () {
     restartButton.classList.add('hide');
     questionCont.classList.add('hide');
     letsStart.classList.remove('hide');
@@ -42,14 +39,14 @@ restartButton.onclick = function() {
  * hides the start button, shuffles the questions amd adds 10
  * moves onto the first question
  */
- function runGame() {
-    
+function runGame() {
+
     letsStart.classList.add('hide');
     shuffledQuestions = questions.sort(() => .5 - Math.random()).slice(0, 10);
     currentQuestionIndex = 0;
     questionCont.classList.remove('hide');
     getNextQuestion();
-    
+
 }
 
 /**
@@ -58,7 +55,7 @@ restartButton.onclick = function() {
 function getNextQuestion() {
     defaultState();
     displayQuestion(shuffledQuestions[currentQuestionIndex]);
-    
+
 
 }
 
@@ -94,13 +91,13 @@ function defaultState() {
  * increments incorrect score if wrong
  * highlights colours for right and wrong buttons
  */
- function checkAnswer(event) {
+function checkAnswer(event) {
     const clickedButton = event.target;
     const correct = clickedButton.dataset.correct;
     setStatusClass(document.body, correct);
     Array.from(answersArea.children).forEach(button => {
         setStatusClass(button, button.dataset.correct);
-    })
+    });
     if (shuffledQuestions.length > currentQuestionIndex + 1) {
         nextButton.classList.remove('hide');
     } else {
@@ -116,9 +113,9 @@ function defaultState() {
 function setStatusClass(element, correct) {
     clearStatusClass(element);
     if (correct) {
-        element.classList.add('correct')
+        element.classList.add('correct');
     } else {
-        element.classList.add('wrong')
+        element.classList.add('wrong');
     }
 }
 
@@ -127,11 +124,12 @@ function clearStatusClass(element) {
     element.classList.remove('wrong');
 }
 
- function incrementScore() {
+function incrementScore() {
     let oldScore = parseInt(document.getElementById("score").innerText);
     document.getElementById("score").innerText = ++oldScore;
 }
- function incrementWrongAnswer() {
+
+function incrementWrongAnswer() {
     let oldScore = parseInt(document.getElementById("incorrect").innerText);
     document.getElementById("incorrect").innerText = ++oldScore;
 }
@@ -141,6 +139,6 @@ function endGame() {
         <strong><em>Quizzical!</em></strong>
         <br>
         Click restart to retry
-        `
-        restartButton.classList.remove('hide');
+        `;
+    restartButton.classList.remove('hide');
 }
